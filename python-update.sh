777ff552955e65f8ef7913b63047839db25eeea2
Pacome Daval <pac.daval63@gmail.com>
2022-01-28 23:50:08 +0100

    premier commit

---
+++ python-update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Ce script va mettre a jour python3 vers une version plus récente"
+echo 'Quel est votre version actuel?' 
+echo 'Si vous ne savez pas, arrêter le script et lancer la commande "python3 -V"'
+read anc_version
+echo "L'ancienne version est une derivé de la version ? "${anc_version:0:3} "Y/n"
+read answ
+if [ "$answ" == "y" ] || [ "$answ" == "" ] || [ "$answ" == "Y" ]; then 
+    anc_version=${anc_version:0:3} 
+    echo $anc_version
+else
+    echo "Est-ce un derivé de cette version "${anc_version:0:4} "Y/n"
+    read rep
+    if [ "$rep" == "y" ] || [ "$rep" == "" ] || [ "$rep" == "Y" ]; then
+    anc_version=${anc_version:0:4} 
+    echo $anc_version 
+    else
+        echo "relancez le programme!"
+    fi
+fi
+echo "Quel version voulez vous télécharger"
+read version
+echo "Voulez-vous télécharger la version" $version "?" "Y/n"
+read reponse
+if [ "$reponse" == "y" ] || [ "$reponse" == "" ] || [ "$reponse" == "Y" ]; then
+    echo "J'installe la version " $version
+    sudo apt-get update
+    sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev tar wget
+    wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+    tar zxf Python-$version.tgz
+    cd Python-$version
+    sudo ./configure --enable-optimizations
+    sudo make -j 4
+    sudo make altinstall
+    cd ../
+    rm -r Python-$version
+    rm Python-$version.tar.xz
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python$anc_version 0
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python${version:0:4} 1
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 0
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+    sudo ln -s /usr/share/pyshared/lsb_release.py /usr/local/lib/python${version:0:4}/site-packages/lsb_release.py
+else
+    echo "Recommencez le script"
+fi
